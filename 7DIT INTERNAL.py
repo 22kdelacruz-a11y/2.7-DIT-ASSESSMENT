cc33a148a9f597b14027ff7959e296de4c8578c7
Kian Dela Cruz <22kdelacruz@wakatipu.school.nz>
2026-04-24 15:05:27 +1200

    added the menu system for both customers and the owner and refined the code for the get_user function.

--- 7DIT INTERNAL.py
+++ 7DIT INTERNAL.py
@@ -1,30 +1,68 @@
 #list of lists (lol)
-password = "ilovpotle"
-
-def staff_program():
-    pass
+password = "pw"
+orders = []
 
 
-def get_user():     #a function to check if the one handling the website is a customer or an employee/staff
+#functions
+def menu():
+    print("1 - Make your own!")
+    print("2 - Nachorrrito")
+    print("3 - Special Bowl!!!")
+    print("4")
+
+def customer_program():
+    print("What would you like to do?")
+    print("1. View menu.")
+    print("2. Order.")
+    print("3. Exit.")
+
+def owner_program():
+    print("What would you like to do?")
+    print("1. View orders.")
+    print("2. Cancel orders")
+    print("3. Exit.")
+
+def get_user():  #a function to check if the one handling the website is a customer or an employee/staff
+    print()
+    print("               -------------------------")
+    print("Welcome to n/a! Are you the owner or a customer?")
     while True:
-        user = input("staff or customer: ").lower()            #all of the printed statements are temporary
+        user = input(": ").lower()   #all of the printed statements are temporary
         if user == "customer":
             return user
-        elif user == "staff":
-            print("In order to access the staff thingy you need to enter a password.")      #password system
-            pw_input = input("Enter the password: ")        
+        elif user == "owner":
+            print()
+            print("In order to access the owners account, you would need to enter a password. To return, input X.")      #password system
+            pw_input = input("Enter the password: ").lower()     
             if pw_input == password:
+                print()
+                print("               -------------------------")
                 print("Welcome!")
                 return user
+            if pw_input == "x":
+                print()
+                print("               -------------------------")
+                print("Welcome to n/a! Are you  the owner or a customer?")
+                continue
             else:
-                print("wrong")
+                print()
+                print("Incorrect password. Try again.")
+                
         else:
-            print('You must input either "Customer" or "Staff", Try again.')
+            print()
+            print('You must input either "Customer" or "Owner", Try again.')
+            
 
 
 
 user = get_user()
 
-print(user)
-if user == "staff":
-    staff_program()
+if user == "owner":
+    owner_program()
+owner_input = int(input(": "))
+
+if user == "customer":
+    customer_program()
+    
+
+
